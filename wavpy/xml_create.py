f9bba071d86e5c083de3b2a4ce5b4564c782e794
Rohit Bisht <rbtunes0@gmail.com>
2022-09-04 15:58:07 +0000

    updated

--- wavpy/xml_create.py
+++ wavpy/xml_create.py
@@ -1,5 +1,5 @@
 #file_name = "malhar_time.txt"
-with open('time_o', 'r') as fr:
+with open('time_o.txt', 'r') as fr:
     # reading line by line
     lines = fr.readlines()
     lines = lines[:-1] 
@@ -7,20 +7,22 @@ with open('time_o', 'r') as fr:
     ptr = 1
   
     # opening in writing mode
-    with open('time', 'w') as fw:
+    with open('time.txt', 'w') as fw:
         for line in lines:
-            
+            # y = line.replace('created','')
+            y = line.replace('.mp3','_')
             # we want to remove 5th line
             if ptr not in [1,2,3,4,5,6,7,8,9]:
-                fw.write(line)
+                fw.write(y)
             ptr += 1
 
-file_name = "./time"
+file_name = "time.txt"
 
 time_list = []
 with open(file_name, 'r', encoding='utf-8',  errors='ignore') as f:
     for line in f:
-        time = line.split("\n")[0].split("_")[1]
+        time = line.split("\n")[0].split("_")[2]
+        # time = time.replace('created" ','')
         time_list.append(time)
 print(len(time_list))
 
@@ -35,6 +37,10 @@ print(len(sentence_list))
 file_name = "G_2.xml"
 wfile = open(file_name, 'w+', encoding='utf-8')
 i = 0
+#<?xml version="1.0" encoding="UTF-8"?>
+# <transcript lang="english">
+wfile.write('<?xml version="1.0" encoding="UTF-8"?>'+"\n")
+wfile.write('<transcript lang="english">'+"\n")
 for j in range(len(sentence_list)):
 	sentence = sentence_list[j]
 	time = time_list[j]
@@ -43,4 +49,4 @@ for j in range(len(sentence_list)):
 	for k in range(len(word)):
 		wfile.write("<word timestamp=\"\" is_valid=\"1\">"+str(word[k])+"</word>"+"\n")
 	wfile.write("</line>"+"\n")
-
+wfile.write('</transcript>')
