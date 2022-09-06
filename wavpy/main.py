43403f089b9f5a7041a512192a23014ee4136635
Rohit Bisht <rbtunes0@gmail.com>
2022-09-06 18:56:15 +0000

    c try

--- wavpy/main.py
+++ wavpy/main.py
@@ -1,12 +1,20 @@
 from flask import Flask ,request
 import os
-
+UPLOAD_FOLDER = './uploads'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/",methods=["POST","GET"])
 def home():
     if request.method=="POST":
-        print(request.files.get('file'))
+        # print(request.get_data())
+        print( request.mimetype)
+        file = request.data
+        
+        with open('./uploads/a.txt', 'w') as f:
+            f.write(str(file))
+        # file = request.files['file']
+        # print(file.filename)
         # file = request.files.get('file')
         # if file:
         #     mimetype = file.content_type
